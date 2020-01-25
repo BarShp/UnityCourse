c812f92fd0ce2c58b6bcf6a33a83cc6f2cc6f5cd
barshp <bars@sela.co.il>
2020-01-25 15:53:02 +0200

    Lesson2 after lesson

---
+++ Lesson2/Assets/Scripts/Cactus.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cactus : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("Meow");
+        other.gameObject.SetActive(false);
+    }
+
+}
---
+++ Lesson2/Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform player;
+    public float height;
+    public float distance;
+
+    private Vector3 offest;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offest = new Vector3(0, height, -distance) - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position + offest;
+        transform.LookAt(player);
+    }
+}
