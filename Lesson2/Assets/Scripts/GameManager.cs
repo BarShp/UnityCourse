0d3eb9f1cb8f566347e40a9c22abba430c23e708
barshp <>
2020-01-29 23:41:51 +0200

    Lesson 3 push

---
+++ Lesson2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Text scoreText;
+    public GameObject gameOverCanvas;
+
+    static private int score;
+
+    void Start()
+    {
+        score = 0;
+        scoreText.text = GetScoreText();
+        gameOverCanvas.SetActive(true);
+    }
+
+    public void AddScore(int amount)
+    {
+        score += amount;
+        scoreText.text = GetScoreText();
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private string GetScoreText() => $"Score: {score}";
+}
