cee23c4be36d080d97ec28f1e9e2882926b78a02
barshp <>
2020-02-12 00:22:41 +0200

    Lesson 4

---
+++ Lesson4/Assets/Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Transform objectToSpawn;
+
+    void Start()
+    {
+        Instantiate(objectToSpawn, transform);
+        InvokeRepeating("Spawn", 0, 1);
+    }
+
+    void Update()
+    {
+
+    }
+
+    void Spawn()
+    {
+        Instantiate(objectToSpawn, transform);
+    }
+}
